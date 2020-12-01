8dec0dcf02656d566e52ae5e58b2827a003a9f7b
Diehl Wierce <johnbrotherwood@gmail.com>
2020-12-02 01:15:35 +0300

    Third commit

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoUri: process.env.MONGO_URI,
-    PORT: process.env.PORT || 3000,
-}
+  mongoUri:
+    "mongodb+srv://admin:extention1232@cluster0.ukrvz.mongodb.net/test?retryWrites=true&w=majority", //process.env.MONGO_URI,
+  PORT: process.env.PORT || 3000,
+};
--- routes/api/bucketListItems.js
+++ routes/api/bucketListItems.js
@@ -1,4 +1,4 @@
-const { Router } = require("mongoose");
+const { Router } = require("express");
 const BucketListItem = require("../../models/BucketListItem");
 
 const router = Router();
